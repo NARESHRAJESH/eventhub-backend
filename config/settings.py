c9b1f076c1889c8aedb8d102ae7a61aba79457bd
NARESHRAJESH <nareshrajesh2003@gmail.com>
2026-09-17 18:02:24 +0530

    Remove DEFAULT_FILE_STORAGE - use STORAGES only

--- config/settings.py
+++ config/settings.py
@@ -132,8 +132,6 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
 
 # CORS  ← IDHU THAAN FIX (CORS, not CORSCORS)
 CORS_ALLOWED_ORIGINS = [
