f90ef0abfcf578302ae6c80e8b19934775e08870
NARESHRAJESH <nareshrajesh2003@gmail.com>
2026-09-17 17:32:27 +0530

    Remove deprecated DEFAULT_FILE_STORAGE

--- config/settings.py
+++ config/settings.py
@@ -119,7 +119,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
